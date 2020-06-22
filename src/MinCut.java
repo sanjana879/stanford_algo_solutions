ba0f42c0bdd37f2444e09c03ec8d9dad8d483c76
sanjana879 <yadavs1@go.lisd.net>
2020-06-22 15:16:08 -0500

    cleaned

--- src/MinCut.java
+++ src/MinCut.java
@@ -1,192 +1,254 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
 
 public class MinCut 
 {
-	public int countElements(Node[][] arr)
+	int smallestNum = Integer.MAX_VALUE;
+	Graph smallest;
+	
+	public Vertex findVertex(HashMap<Vertex, List<Vertex>> adjList, int val)
 	{
+		Iterator<Vertex> list = adjList.keySet().iterator();
+		while(list.hasNext())
+		{
+			Vertex c = list.next();
+			if(c.val == val)
+				return c;
+		}
+		return null;
+	}
+	
+	public void repeat(Graph g)
+	{
+		int times = g.adjList.keySet().size();
+		
+		for(int i = 0; i < times*times;i++)
+		{
+			Graph original = new Graph();
+			//System.out.println(i + " " + original);
+			original.adjList.putAll(g.adjList);
+			
+			Graph newG = new Graph( g.copy(g.adjListCloned));
+			//System.out.println(i );
+			findMin(newG);
+			
+		}
+		System.out.println(smallestNum);
+	}
+	
+	public Graph findMin(Graph g)
+	{
+		int size = g.adjList.size();
+		int numEdges = countEdges(g.adjList);
+		while(size > 2)
+		{
+			 int rand = (int) ((Math.random() * (numEdges - 1)) + 1);
+			 //System.out.println(size);
+			 Vertex[] returned = findVertices(g.adjList,rand);
+			 //System.out.println(returned[0] + " " + returned[1]);
+			 g.merge(returned[0], findVertex(g.adjList,returned[1].val));
+			 size --;
+			 numEdges = countEdges(g.adjList);
+			 //if(numEdges < 2)
+				 //System.out.println(returned[0] + " " + returned[1]);
+				 //System.out.println(g);
+		}
+		if(numEdges < smallestNum)
+		{
+			smallestNum = numEdges;
+			smallest = new Graph(g.adjList);
+			/*if(smallestNum < 2)
+				System.out.println(smallest);
+			if(smallestNum == 2)
+				System.out.println(smallest);*/
+		}
+		
+		return new Graph(g.adjList);
+	}
+	
+	public Vertex[] findVertices(HashMap<Vertex, List<Vertex>> adjList, int num)
+	{
+		int copy = num;
+		//System.out.println("ind " + num + " " + g);
+		Vertex[] returned = new Vertex[2];
+		Iterator<Vertex> keySet = adjList.keySet().iterator();
+		Iterator<List<Vertex>> valSet = adjList.values().iterator();
+		Vertex curr = keySet.next();
+		List<Vertex> currList = valSet.next();
+		num -= currList.size();
+		while(keySet.hasNext() && valSet.hasNext()  && num > 0)
+		{
+			curr = keySet.next();
+			currList = valSet.next();
+			num -= currList.size();
+
+			
+		}
+		//System.out.println(adjList + " " + copy + " " + num);
+		if(num <= 0)
+		{
+			returned[0] = curr;
+			//System.out.println(currList.size()+num-1 + " " +currList.get(currList.size()+num-1));
+			returned[1] = (currList.get(currList.size()+num-1));
+		}
+		//System.out.println(num + " " + curr.val + currList.toString() + " " + returned[0] + " " + returned[1]);
+		return returned;
+	}
+	
+
+	
+	public int countEdges(HashMap<Vertex, List<Vertex>> adjList)
+	{
+		Iterator<List<Vertex>> valSet =adjList.values().iterator();
 		int count = 0;
-		for(int i = 0; i < arr.length; i++)
+		while(valSet.hasNext())
 		{
-			count+= arr[i].length-1;
-		}
-		return count;
-	}
-
-	public static String print(Node[][] arr)
-	{
-		String output = "";
-		for(int i = 0;i<arr.length;i++)
-		{
-			//System.out.println(output);
-			for(int j = 0;j<arr[i].length;j++)
-				output += arr[i][j].toString();
-			output += '\n';
-		}
-		return output;
-	}
-	public static Node[][] contract(Node[][] arr, int big, int small, int ind)
-	{
-		System.out.println(print(arr));
-		int newLen = arr[big].length-1+arr[small].length-2;
-		Node smallVal = arr[small][0];
-		Node bigVal = arr[big][0];
-		Node[][] newArr = new Node[arr.length-1][];
-		int newRow = 0;
-		for(int i =0;i<arr.length;i++)
-		{
-			if(i != big && i != small)
-			{
-				newArr[newRow] = new Node[arr[i].length];
-				newArr[newRow][0] = arr[i][0];
-				int newInd = 1;
-				//copy old values
-				for(int j = 1; j < arr[i].length;j++)
-				{
-					if(arr[i][j].val != smallVal.val)
-					{
-						newArr[newRow][newInd] = arr[i][j];
-						newInd++;
-					}
-					else if(arr[i][j].val == smallVal.val)
-					{
-						newArr[newRow][newInd] = new Node(-1);
-						newInd++;
-					}
-				}
-				newRow++;
-			}
-			else if(i == big)
-			{
-				newArr[newRow] = new Node[newLen];
-				newArr[newRow][0] = arr[i][0];
-				int newInd = 1;
-				//copy old values
-				for(int j = 1; j < arr[i].length;j++)
-				{
-					if(arr[i][j].val != smallVal.val && arr[i][j].val != -1)
-					{
-						newArr[newRow][newInd] = arr[i][j];
-						newInd++;
-					}
-				}
-				//copy new values
-				for(int j = 1; j < arr[small].length;j++)
-				{
-					if(arr[small][j].val != bigVal.val)
-					{
-						newArr[newRow][newInd] = arr[small][j];
-						newInd++;
-					}
-				}
-				System.out.println("fngkafsa " + big + " " + Arrays.toString(newArr[newRow]) + smallVal.val);
-				newRow++;
-			}
-		}
-		System.out.println(print(newArr));
-		return newArr;
-	}
-	public Node[][] calcCut(Node[][] arr)
-	{
-		int total = countElements(arr);
-		while(arr.length > 2)
-		{
-			int ind = (int) ((Math.random() * (total - 1)) + 1);
-			int row = 0;
-			while(ind / arr[row].length > 0)
-			{
-				ind %= arr[row].length;
-				row++;
-			}
-			
+			int size = valSet.next().size();
+			count += size;
+			//if(size == 0)
+				//count += 1;
 			
 		}
-		
-		
-		return arr;
+		return count/2;
 	}
-	public String copy(int n, int k)
-    {
-        StringBuilder ans = new StringBuilder();
-        ArrayList<Integer> list = populate(n);
-        int factor = factorial(n);
-        int copyN = n;
-        int first = 0;
-        while(copyN > ((factor)/n)) //6
-        {
-            first++; 
-            copyN -= (factor/n);
-        }
-        ans.append(String.valueOf(list.get(first)));
-        System.out.println(first);
-        list.remove(first);
-        System.out.println(ans.toString());
-        int second = 0;
-        while(copyN > ((factor)/(n*(n-1)))) //2
-        {
-            second++;
-            copyN -= factor/(n*(n-1));
-        }     
-        ans.append(String.valueOf(list.get(second)));
-        list.remove(second);
-        System.out.println(ans.toString());
-        int third = 0;
-        while(copyN > ((factor)/(n*(n-1)*(n-2))))
-        {
-            third++;
-            copyN -= factor/(n*(n-1)*(n-2));
-        }
-        ans.append(String.valueOf(list.get(third)));
-        list.remove(third);
-        System.out.println(ans.toString());
-        for (int s : list)
-        {
-            ans.append(String.valueOf(s));
-            //sb.append("\t");
-        }
-
-        //ans += String.join("", list);
-        
-        /*
-            n = 4. 1st - 6, 
-            1st - n! / n
-            1234, 1243, 1324, 1342, 1423, 1432, 2134, 2143, 2314, 2341, 2413, 2431
-            4*3*2 = 24
-            
-        */
-        
-        
-        
-        
-        return ans;
-    }
- 	public static void main(String[] args) 
+	
+	public Graph makeGraph(Scanner s)
 	{
-		// TODO Auto-generated method stub
- 		Node[][] arr = {{new Node(1),new Node(2),new Node(3)},
- 						{new Node(2),new Node(1),new Node(3),new Node(4)},
- 						{new Node(3),new Node(1),new Node(2),new Node(4)},
- 						{new Node(4),new Node(2),new Node(3)},
- 						};
- 				/*1 2 3
- 				2 1 3 4 
- 				3 1 2 4
- 				4 2 3 */
- 		contract(arr,0,2,2);
- 		contract(arr,0,1,1);
- 	}
+		HashMap<Vertex, List<Vertex>> list = new HashMap<Vertex, List<Vertex>>();
+		while(s.hasNextLine())
+		{
+			String[] input = s.nextLine().split(" " );
+			Vertex key = new Vertex(Integer.parseInt(input[0]));
+			List<Vertex> values = new ArrayList<Vertex>();
+			for(int i = 1; i < input.length;i++)
+			{
+				values.add(new Vertex(Integer.parseInt(input[i])));
+			}
+			list.put(key, values);
+		}
+		//System.out.println(list);
+		Graph g = new Graph(list);
+		//System.out.println(list.toString());
+		return g;
+	}
+	
+	public static void main(String[] arg) throws FileNotFoundException
+	{
+		MinCut min = new MinCut();
+		Scanner s = new Scanner(new File("minCut.txt"));
+		Graph g = min.makeGraph(s);
+		min.repeat(g);
+		
+	}
 }
 
-
-
-
-class Node {
+class Graph
+{
+	HashMap<Vertex, List<Vertex>> adjList;
+	HashMap<Vertex, List<Vertex>> adjListCloned;
+	int edges;
+	public String toString()
+	{
+		return adjList.toString();
+	}
+	
+	public Vertex findVertex(int val)
+	{
+		Iterator<Vertex> list = adjList.keySet().iterator();
+		while(list.hasNext())
+		{
+			Vertex c = list.next();
+			if(c.val == val)
+				return c;
+		}
+		return null;
+	}
+	public HashMap <Vertex, List<Vertex>> copy(HashMap<Vertex, List<Vertex>> original)
+	{
+		//HashMap<Vertex, List<Vertex>> original = adjList;
+		//System.out.println(original);
+		    HashMap<Vertex, List<Vertex>> copy = new HashMap<Vertex, List<Vertex>>();
+		    for (Vertex entry : original.keySet())
+		    {
+		    		//System.out.println(entry);
+		        copy.put(new Vertex(entry.val),
+		           // Or whatever List implementation you'd like here.
+		           new ArrayList<Vertex>(original.get(entry)));
+		    }
+		    return (copy);
+	}
+	public void merge(Vertex a, Vertex b)
+	{
+		//System.out.println();
+		//add new values & remove key
+		List<Vertex> aList = adjList.get(a);
+		List<Vertex> bList = adjList.get(b);
+		//System.out.println(aList);
+		//System.out.println(adjList.get(b));
+		bList.forEach((value) -> {
+			if(value.val != b.val) 
+				aList.add(value);
+			List<Vertex> temp = adjList.get(findVertex(value.val));
+			temp.add(a);
+		});
+		
+		Iterator<Vertex> keySet = adjList.keySet().iterator();
+		while(keySet.hasNext())
+		{
+			Vertex v = keySet.next();
+			List<Vertex> currList = adjList.get(v);
+			ArrayList<Vertex> toRemove = new ArrayList<Vertex>();
+			currList.forEach((value) -> {
+				//remove self loops
+				if(value.val == b.val || value.val == v.val) 
+					toRemove.add(value);
+					//currList.remove(value);
+			});
+			for(int i = 0; i < toRemove.size();i++)
+				currList.remove(toRemove.get(i));
+		}
+		adjList.remove(b);
+	}
+	public void countEdges(Graph g)
+	{
+		Iterator<List<Vertex>> valSet = g.adjList.values().iterator();
+		int count = 0;
+		while(valSet.hasNext())
+		{
+			count += valSet.next().size();
+		}
+		edges = count;
+	}
+	public Graph(HashMap<Vertex, List<Vertex>> a)
+	{
+		adjList = a;
+		//System.out.println(a);
+		adjListCloned = copy(a);
+	}
+	public Graph()
+	{
+		adjList = new HashMap<Vertex, List<Vertex>>();
+	}
+	
+}
+class Vertex
+{
 	int val;
-	public Node(int v)
+	public Vertex(int v)
 	{
 		val = v;
 	}
 	public String toString()
 	{
-		return String.valueOf(val) + " ";
+		return String.valueOf(val);
 	}
 }
